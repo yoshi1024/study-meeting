71740d8abab2b55ac3d76825b4ca13e6375bc6ce
yoshida_ibuki_windows_tourism <yoshida-ibuki@ed.tmu.ac.jp>
2022-12-12 01:20:38 +0900

    1212

--- 勉強会/第3回/Pythonコード.py
+++ 勉強会/第3回/Pythonコード.py
@@ -15,3 +15,44 @@ df = df[df['西暦（年）'] == 2015]
 
 #都道府県データのみ抽出
 df = df[~df['都道府県コード'].isin(['00','0A','0B'])]
+
+df['都道府県コード'] = df['都道府県コード'].astype(int)
+df['人口（男）'] = df['人口（男）'].astype(int)
+df['人口（総数）'] = df['人口（総数）'].astype(int)
+
+df_touhoku = df[df.都道府県コード < 8]
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+
+plt.bar(df_touhoku['都道府県名'], df_touhoku['人口（総数）'])
+
+pip install japanize-matplotlib
+
+import japanize_matplotlib
+import matplotlib.pyplot as plt
+
+plt.bar(df_touhoku['都道府県名'], df_touhoku['人口（総数）'])
+
+plt.scatter(
+pd.Series(df['人口（男）'],dtype='int'),
+pd.Series(df['人口（女）'],dtype='int'))
+
+pd.Series(df['人口（男）'],dtype='int')
+
+plt.scatter(
+pd.Series(df['人口（男）'],dtype='int'),
+pd.Series(df['人口（女）'],dtype='int'))
+
+plt.title('Population')
+plt.xlabel('man')
+plt.ylabel('woman')
+
+plt.show()
+
+# 箱ひげ図
+plt.boxplot(df['人口（男）'])
+plt.show()
+
+plt.hist(df['人口（男）'])
+plt.show()
--- 勉強会/第3回/Rコード.r
+++ 勉強会/第3回/Rコード.r
@@ -15,3 +15,27 @@ df <- df[
     (df$都道府県コード!='0A') &
     (df$都道府県コード!='0B'),
 ]
+
+df$都道府県コード <- as.numeric(df$都道府県コード)
+
+df$人口.総数. <- as.numeric(df$人口.総数.)
+df$人口.男. <- as.numeric(df$人口.男.)
+df$人口.女. <- as.numeric(df$人口.女.)
+
+df_touhoku <- df[df$都道府県コード < 8 ,]
+
+# ggplot2パッケージ呼び出し
+library(ggplot2)
+
+# 作図
+ggplot(data = df_touhoku, aes(x = 都道府県名, y = 人口.総数.)) + 
+  geom_bar(stat = "identity") 
+
+plot(df[,'人口.男.'],df[,'人口.女.'])
+
+ggplot(data = df, mapping = aes(x = df[,'人口.男.'], y = df[,'人口.女.'])) +
+  geom_point()
+
+boxplot(df$人口.男.)
+
+hist(df$人口.男.)
